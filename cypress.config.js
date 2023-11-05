6a831d68d9acaea6c27dfccc2ac02149d4b98210
performanceserve <performanceserve@gmail.com>
2023-11-05 16:01:44 +0200

    install component tests + lighthouse module

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,21 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://bstackdemo.com", // this is your app
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    on("before:browser:launch", (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+    });
+    on("task", {
+    lighthouse: lighthouse(),
+    // pa11y: pa11y(console.log.bind(console)),
+    });
     },
   },
 
   component: {
     devServer: {
-      framework: "create-react-app",
+      framework: "react",
       bundler: "webpack",
     },
   },
